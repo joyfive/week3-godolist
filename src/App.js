b32e162323c89a69e023af886a417681de5e2f30
joyfive <joy_five@kakao.com>
2022-10-01 22:40:18 +0900

    update error } :(

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   const onClick = () => setValue((prev) => prev + 1);
   return (
   <div>
-    <h1>{counter</h1>
+    <h1>{counter}</h1>
     <button onClick={onClick}>click me</button>
   </div>
   );
