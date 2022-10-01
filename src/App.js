05ec2ed0dba420219932141279e1351f62c751f7
joyfive <joy_five@kakao.com>
2022-10-01 18:46:46 +0900

    update app.js / props 기본형 버튼 컴포넌트 연습

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import logo from './logo.svg';
+
 import './App.css';
 
+function Btn(props) {
+  return <button style={{
+    backgroundColor: "tomato",
+    color:"white",
+    padding: "10px 20px",
+    border: 0,
+    borderRadius: 10,
+  }}>
+    {props.text}
+    </button>
+}
 function App() {
-  return (
-
+  return (<div>
+    <Btn text="Save Changes" />
+    <Btn text="Countinue" />
+  </div>
   );
 }
 
