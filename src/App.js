6cedc689f0b96d00356c82e1ec3ca2a06a68c2fc
joyfive <joy_five@kakao.com>
2022-10-02 17:04:48 +0900

    RESET todolist first setting

--- src/App.js
+++ src/App.js
@@ -1,32 +1,10 @@
-import { useEffect, useState } from "react";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
 
-function App() {
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeyword] = useState('');
-  const onClick = () => setValue((prev) => prev + 1);
-  const onChange = (event) => setKeyword(event.target.value);
-  console.log('I run all the time.');
-  useEffect(() => {
-    console.log('I run only once');
-  }, []);
-  useEffect(() => {
-    console.log("I run when 'keyword' changes.");
-  }, [keyword]);
-  useEffect(() => {
-    console.log("I run when 'counter' changes.");
-  }, [counter]);
-  return (
-  <div>
-    <input 
-      value={keyword}
-      onChange={onChange}
-      type="text"
-      placeholer="Search here..."
-    />
-    <h1>{counter}</h1>
-    <button onClick={onClick}>click me</button>
-  </div>
-  );
-}
-
-export default App;
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
--- src/Btn.js
+++ src/Btn.js
@@ -1,5 +1,5 @@
 import PropTypes from "prop-types";
-import sytles from "./Button.module.css";
+import styles from "./Button.module.css";
 
 function Btn({ text }) {
     return <button className={styles.btn}>
@@ -12,3 +12,37 @@ function Btn({ text }) {
   };
 
   export default Btn;
+
+
+//   import { useEffect, useState } from "react";
+
+// function App() {
+//   const [counter, setValue] = useState(0);
+//   const [keyword, setKeyword] = useState('');
+//   const onClick = () => setValue((prev) => prev + 1);
+//   const onChange = (event) => setKeyword(event.target.value);
+//   console.log('I run all the time.');
+//   useEffect(() => {
+//     console.log('I run only once');
+//   }, []);
+//   useEffect(() => {
+//     console.log("I run when 'keyword' changes.");
+//   }, [keyword]);
+//   useEffect(() => {
+//     console.log("I run when 'counter' changes.");
+//   }, [counter]);
+//   return (
+//   <div>
+//     <input 
+//       value={keyword}
+//       onChange={onChange}
+//       type="text"
+//       placeholer="Search here..."
+//     />
+//     <h1>{counter}</h1>
+//     <button onClick={onClick}>click me</button>
+//   </div>
+//   );
+// }
+
+// export default App;
