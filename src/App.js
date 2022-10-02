b4d05a04591deae5cf40c96640643b436c06bd53
joyfive <joy_five@kakao.com>
2022-10-02 10:05:08 +0900

    UPDATE add useState

--- src/App.js
+++ src/App.js
@@ -2,15 +2,23 @@ import { useEffect, useState } from "react";
 
 function App() {
   const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState('');
   const onClick = () => setValue((prev) => prev + 1);
-  console.log('I run all the time.')
-  const useEffect(() => {
-    console.log('CALL THE API');
+  const onChange = (event) => setKeyword(event.target.value);
+  console.log('I run all the time.');
+  useEffect(() => {
+    console.log('CALL THE API...');
   }, []);
   return (
   <div>
+    <input 
+      value={keyword}
+      onChange={onChange}
+      type="text"
+      placeholer="Search here..."
+    />
     <h1>{counter}</h1>
-    <button onClick={onClick}>click me</button>
+    <button onChange={onClick}>click me</button>
   </div>
   );
 }
