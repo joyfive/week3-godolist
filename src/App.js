dc4f4ef3afc01055753d041d2af68a0075d95ca3
joyfive <joy_five@kakao.com>
2022-10-02 09:46:46 +0900

    try useEffect : 매번 리렌더링하지않고, 1번만 실행하기

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
   const [counter, setValue] = useState(0);
   const onClick = () => setValue((prev) => prev + 1);
+  console.log('I run all the time.')
+  const iRunOnlyOnce = () => {
+    console.log('CALL THE API')
+  };
+  useEffect(iRunOnlyOnce, []);
   return (
   <div>
     <h1>{counter}</h1>
