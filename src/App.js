933d5ffcbd926a75a757b9d9a8e162190cd9f745
joyfive <joy_five@kakao.com>
2022-10-02 09:58:02 +0900

    TRY useEffect : 즉시 호출 함수로 작성하기 (shortcut)

--- src/App.js
+++ src/App.js
@@ -4,10 +4,9 @@ function App() {
   const [counter, setValue] = useState(0);
   const onClick = () => setValue((prev) => prev + 1);
   console.log('I run all the time.')
-  const iRunOnlyOnce = () => {
-    console.log('CALL THE API')
-  };
-  useEffect(iRunOnlyOnce, []);
+  const useEffect(() => {
+    console.log('CALL THE API');
+  }, []);
   return (
   <div>
     <h1>{counter}</h1>
