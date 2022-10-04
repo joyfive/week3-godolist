406e844930354c474ffe0ff77d7486586521d17f
joyfive <joy_five@kakao.com>
2022-10-05 01:56:43 +0900

    UPDATE css - tiny something

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -6,7 +6,9 @@ const Layout = ({children}) => {
   <div className="all">
     <div className="layout"> {children} </div>
     <div className="footer">
-        Copyright 2022. hang-hae99 9th W3 @joyfive all rights reserved.
+        <div className="footer-txt">
+        Copyright 2022. hang-hae99 9th W3 @joyfive all rights reserved. 
+        </div>
     </div>
   </div>);
 };
